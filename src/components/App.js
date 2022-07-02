b00462ca5ecb319e89bd4f948ec045dd95e9fca7
HashBlast3r <hashblast3r@gmail.com>
2022-07-02 15:08:50 -0500

    added sign in/out button with google oauth

--- src/components/App.js
+++ src/components/App.js
@@ -5,12 +5,15 @@ import StreamDelete from './streams/StreamDelete';
 import StreamEdit from './streams/StreamEdit';
 import StreamList from './streams/StreamList';
 import StreamShow from './streams/StreamShow';
+import Header from './Header';
+// 194190248286-a4qc19s38855mdv0mnoqqodbkrrpfn73.apps.googleusercontent.com
 
 const App = () => {
   return (
-    <div>
+    <div className='ui container'>
       <BrowserRouter>
         <div>
+          <Header />
           <Route path='/' exact component={StreamList} />
           <Route path='/streams/new' exact component={StreamCreate} />
           <Route path='/streams/edit' exact component={StreamEdit} />
---
+++ src/components/GoogleAuth.js
@@ -0,0 +1,56 @@
+import React from "react";
+
+class GoogleAuth extends React.Component {
+  state = { isSignedIn: null };
+
+  componentDidMount() {
+    window.gapi.load('client:auth2', () => {
+      window.gapi.client.init({
+        clientId: '194190248286-a4qc19s38855mdv0mnoqqodbkrrpfn73.apps.googleusercontent.com',
+        scope: 'email',
+        plugin_name: 'twitchClone'
+      }).then(() => {
+        this.auth = window.gapi.auth2.getAuthInstance();
+        this.setState({ isSignedIn: this.auth.isSignedIn.get() });
+        this.auth.isSignedIn.listen(this.onAuthChange)
+      });
+    });
+  }
+
+  onAuthChange = () => {
+    this.setState({ isSignedIn: this.auth.isSignedIn.get() });
+  };
+
+  onSignInClick = () => {
+    this.auth.signIn();
+  };
+
+  onSignOutClick = () => {
+    this.auth.signOut();
+  };
+
+  renderAuthButton() {
+    if (this.state.isSignedIn === null) {
+      return null;
+    } else if (this.state.isSignedIn) {
+      return (
+        <button onClick={this.onSignOutClick} className="ui green google button">
+          <i className="google icon" />
+          Sign Out
+        </button>);
+    } else {
+      return (
+        <button onClick={this.onSignInClick} className="ui red google button">
+          <i className="google icon" />
+          Sign In With Google
+        </button>
+      );
+    }
+  }
+
+  render() {
+    return <div>{this.renderAuthButton()}</div>;
+  }
+}
+
+export default GoogleAuth;
